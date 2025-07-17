dca6479d550e914b19e5570a1fc9cfdcfa9c8f53
ankitrosh <41516377+ankitrosh@users.noreply.github.com>
2025-07-17 14:02:17 -0700

    Time: 145 ms (8.26%), Space: 70.4 MB (39.52%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -3,20 +3,22 @@ public:
     int maxOperations(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        int i = 0;
-        int j = n-1;
         int num_ops = 0;
-        while(j > i){
-            if(nums[i] + nums[j] == k){
+        unordered_map<int, int> umap;
+        for(int i =0; i< n;i++){
+            if(umap.find(k - nums[i]) != umap.end()){
                 num_ops++;
-                i++;
-                j--;
-            } else if(nums[i] + nums[j] < k){
-                i++;
+                umap[k - nums[i]] -= 1;
+                if(umap[k-nums[i]] == 0){
+                    umap.erase(k-nums[i]);
+                }
+            } else if(umap.find(nums[i]) != umap.end()){
+                umap[nums[i]] += 1;
             } else {
-                j--;
+                umap.emplace(nums[i], 1);
             }
         }
+       
 
         return num_ops;
 
