7517dd5066043672c631b95bc12048063a24eff1
ankitrosh <41516377+ankitrosh@users.noreply.github.com>
2025-07-17 14:18:23 -0700

    Time: 109 ms (36.17%), Space: 71.9 MB (31.59%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -4,15 +4,12 @@ public:
         int n = nums.size();
         int num_ops = 0;
         unordered_map<int, int> umap;
-        umap.reserve(nums.size());                 // 1) allocate all buckets up‑front
-        umap.max_load_factor(0.7f); 
         for(int i =0; i< n;i++){
-            auto it = umap.find(k - nums[i]);
-            if(it != umap.end()){
+            if(umap.find(k - nums[i]) != umap.end()){
                 num_ops++;
-                // it->second -= 1;
-                if(--it->second == 0){
-                    umap.erase(it);
+                umap[k - nums[i]]--;
+                if(umap[k-nums[i]] == 0){
+                    umap.erase(k-nums[i]);
                 }
             }  else {
                umap[nums[i]]++;
