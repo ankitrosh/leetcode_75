4cd3cc319ffa5cd5f022ae2336bca02f269bd6e7
ankitrosh <41516377+ankitrosh@users.noreply.github.com>
2025-07-17 14:05:48 -0700

    Time: 96 ms (51.1%), Space: 71.9 MB (31.59%) - LeetHub

--- 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.cpp
@@ -7,20 +7,16 @@ public:
         for(int i =0; i< n;i++){
             if(umap.find(k - nums[i]) != umap.end()){
                 num_ops++;
-                umap[k - nums[i]] -= 1;
+                umap[k - nums[i]]--;
                 if(umap[k-nums[i]] == 0){
                     umap.erase(k-nums[i]);
                 }
-            } else if(umap.find(nums[i]) != umap.end()){
-                umap[nums[i]] += 1;
-            } else {
-                umap.emplace(nums[i], 1);
+            }  else {
+               umap[nums[i]]++;
             }
         }
-       
 
         return num_ops;
-
 
     }
 };
