7368edccfec160ed388d8689aec7a04a582a5c3b
ankitrosh <41516377+ankitrosh@users.noreply.github.com>
2025-07-19 06:27:08 -0700

    Time: 1 ms (29.13%), Space: 36.9 MB (15.54%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
          int n = nums.size();
-    if (n == 0) return -1;
+        if (n == 0) {
+            return -1;
+        };
 
-    vector<int> prefixes(n);
-    prefixes[0] = nums[0];
-    for (int i = 1; i < n; ++i) {
-        prefixes[i] = prefixes[i - 1] + nums[i];
-    }
-
-    vector<int> suffixes(n);
-    suffixes[n - 1] = nums[n - 1];
-    for (int i = n - 2; i >= 0; --i) {
-        suffixes[i] = suffixes[i + 1] + nums[i];
-    }
-
-    for (int i = 0; i < n; ++i) {
-        int leftSum  = (i > 0 ? prefixes[i - 1] : 0);
-        int rightSum = (i + 1 < n ? suffixes[i + 1] : 0);
-        if (leftSum == rightSum) {
-            return i;
+        vector<int> prefixes(n);
+        prefixes[0] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            prefixes[i] = prefixes[i - 1] + nums[i];
         }
-    }
 
-    return -1;
+        vector<int> suffixes(n);
+        suffixes[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            suffixes[i] = suffixes[i + 1] + nums[i];
+        }
+
+        for (int i = 0; i < n; ++i) {
+            int leftSum  = (i > 0 ? prefixes[i - 1] : 0);
+            int rightSum = (i + 1 < n ? suffixes[i + 1] : 0);
+            if(prefixes[i] - nums[i] == suffixes[i] -nums[i]){
+                return i;
+            }
+        }
+
+        return -1;
     }
 };
