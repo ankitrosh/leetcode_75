7b75ed247e9f48628c088de7741336cadf17a88a
ankitrosh <41516377+ankitrosh@users.noreply.github.com>
2025-07-24 00:47:26 -0700

    Time: 15 ms (1.15%), Space: 16.1 MB (5.29%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        }
+        ListNode* curr = head;
+        ListNode* res = new ListNode(curr->val, NULL);
+        ListNode* temp = res;
+        while(curr != NULL && curr->next != NULL){
+            // cout << res->val << endl;
+            // cout << curr->val << endl;
+            // res->next = curr->next->next;
+            
+            // res = res->next;
+            curr = curr->next->next;
+            if(curr){
+                res->next = new ListNode(curr->val, NULL);
+                res= res->next;
+            }
+            
+        }
+        curr = head->next;
+        res->next = new ListNode(curr->val, NULL);
+        res = res->next;
+        while(curr != NULL && curr->next != NULL){
+            cout << curr->val << endl;
+            curr = curr->next->next;
+            if(curr){
+                res->next = new ListNode(curr->val, NULL);
+                res= res->next;
+            }
+        }
+
+
+        
+        
+        return temp;
+
+    }
+};
