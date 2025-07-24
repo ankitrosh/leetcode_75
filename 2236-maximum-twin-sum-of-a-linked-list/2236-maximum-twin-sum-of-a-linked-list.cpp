335a7f6e83261ff473c5c5af3d80533eeffa9b20
ankitrosh <41516377+ankitrosh@users.noreply.github.com>
2025-07-24 01:41:31 -0700

    Time: 4 ms (58.5%), Space: 124.5 MB (58.01%) - LeetHub

--- 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/2236-maximum-twin-sum-of-a-linked-list.cpp
@@ -11,20 +11,48 @@
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        vector<int> twinSums;
+        // vector<int> twinSums;
+
+        // ListNode* curr = head;
+        // int n =0;
+        // while(curr != NULL){
+        //     twinSums.push_back(curr->val);
+        //     curr = curr->next;
+        //     n++;
+
+        // }
+        // int res = twinSums[0] + twinSums[n-1];
+        // for(int i = 1; i < n/2; i++){
+        //     res = max(res, twinSums[i] + twinSums[n-i-1]);
+        // }
+        // return res;
+
+        // no extra space
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        int res = 0;
+
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* nextNode = NULL;
+        ListNode* prev = NULL;
+        while(slow){
+            nextNode = slow->next;
+            slow->next = prev;
+            prev = slow;
+            slow = nextNode;
+        }
 
         ListNode* curr = head;
-        int n =0;
-        while(curr != NULL){
-            twinSums.push_back(curr->val);
+        while(prev){
+            res = max(res, curr->val + prev->val);
+            prev = prev -> next;
             curr = curr->next;
-            n++;
+        }
 
-        }
-        int res = twinSums[0] + twinSums[n-1];
-        for(int i = 1; i < n/2; i++){
-            res = max(res, twinSums[i] + twinSums[n-i-1]);
-        }
         return res;
     }
 };
