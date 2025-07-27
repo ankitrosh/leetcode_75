a41b8a50a06df959d1a93f0c7ccba846d005471a
ankitrosh <41516377+ankitrosh@users.noreply.github.com>
2025-07-27 14:40:17 -0700

    Time: 2 ms (9.97%), Space: 15 MB (61.43%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -8,7 +8,45 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+//  */
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         vector<int> rightView;
+        
+//         if(!root){
+//             return rightView;
+//         }
+//         deque<TreeNode*> q;
+
+//         q.push_front(root);
+//         while(!q.empty()){
+//             int len = q.size();
+//             TreeNode* rightNode = q.back();
+//             rightView.push_back(rightNode->val);
+//             for(int i=0; i < len; i++){
+//                 TreeNode* node = q.front();
+                
+//                 q.pop_front();
+//                 if(node->left){
+//                     q.push_back(node->left);
+
+//                 }
+
+//                 if(node->right){
+//                     q.push_back(node->right);
+//                 }
+//             }
+
+            
+//         }
+
+//         return rightView;
+//     }
+// };
+
+// using a only a queue
+
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
